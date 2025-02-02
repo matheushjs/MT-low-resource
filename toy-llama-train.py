1a5c7cdb6727c7b6051ee35a504877411e35faaa
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2025-02-02 20:31:11 +0900

    Add toy-llama-train.py for testing the waters with BitsAndBytes and Llama

---
+++ toy-llama-train.py
@@ -0,0 +1,20 @@
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    HfArgumentParser,
+    TrainingArguments,
+    pipeline,
+    logging,
+)
+from peft import (
+    LoraConfig,
+    PeftModel,
+    prepare_model_for_kbit_training,
+    get_peft_model,
+)
+import os, torch
+from datasets import load_dataset
+from trl import SFTTrainer, SFTConfig, setup_chat_format
+import bitsandbytes as bnb
+
