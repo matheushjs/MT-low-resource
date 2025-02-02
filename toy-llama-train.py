fe82207ff66873a2c40d07d88235a676406106c4
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2025-02-02 20:33:23 +0900

    Prepare dataset for the model

--- toy-llama-train.py
+++ toy-llama-train.py
@@ -49,3 +49,29 @@ model = AutoModelForCausalLM.from_pretrained(
 
 print(model)
 
+# Load tokenizer
+tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True)
+
+#Importing the dataset
+dataset = load_dataset(dataset_name, split="train")
+
+dataset = dataset.shuffle(seed=65).select(range(1000)) # Only use 1000 samples for quick demo
+
+instruction = """You are a top-rated customer service agent named John. 
+    Be polite to customers and answer all their questions.
+    """
+def format_chat_template(row):
+    row_json = [{"role": "system", "content": instruction },
+               {"role": "user", "content": row["instruction"]},
+               {"role": "assistant", "content": row["response"]}]
+    
+    row["text"] = tokenizer.apply_chat_template(row_json, tokenize=False)
+    return row
+
+dataset = dataset.map(
+    format_chat_template,
+    num_proc=4,
+)
+
+print(dataset)
+
