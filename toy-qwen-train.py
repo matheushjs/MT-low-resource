bafc2af6b40838383b040cf8d8495d150b3fcbd8
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2025-02-04 20:42:13 +0900

    Trainer now work, but lr needs optimization

--- toy-qwen-train.py
+++ toy-qwen-train.py
@@ -72,3 +72,68 @@ dataset = dataset.map(
     num_proc= 4,
 )
 
+def find_all_linear_names(model):
+    cls = bnb.nn.Linear4bit
+    lora_module_names = set()
+    for name, module in model.named_modules():
+        if isinstance(module, cls):
+            names = name.split('.')
+            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+    if 'lm_head' in lora_module_names:  # needed for 16 bit
+        lora_module_names.remove('lm_head')
+    return list(lora_module_names)
+
+modules = find_all_linear_names(model)
+
+# LoRA config
+peft_config = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM",
+    target_modules=modules
+)
+tokenizer.chat_template = None
+model, tokenizer = setup_chat_format(model, tokenizer)
+model = get_peft_model(model, peft_config)
+
+#Hyperparamter
+training_arguments = SFTConfig(
+    output_dir=new_model,
+    per_device_train_batch_size=1,
+    per_device_eval_batch_size=1,
+    gradient_accumulation_steps=2,
+    optim="paged_adamw_32bit",
+    num_train_epochs=1,
+    eval_strategy="steps",
+    eval_steps=0.2,
+    logging_steps=1,
+    warmup_steps=10,
+    logging_strategy="steps",
+    learning_rate=1e-5,
+    fp16=False,
+    bf16=False,
+    group_by_length=True,
+    max_seq_length=512,
+    dataset_text_field="text", # This argument was on Trainer
+    packing= False, # This argument was on Trainer
+    #max_length=512, # Idk what's going on. This is the latest version of TRL but it doesn't seem like it.
+    #report_to="wandb"
+)
+
+dataset = dataset.train_test_split(0.1)
+
+# Setting sft parameters
+trainer = SFTTrainer(
+    model=model,
+    train_dataset=dataset["train"],
+    eval_dataset=dataset["test"],
+    peft_config=peft_config,
+    #max_seq_length=512,
+    #dataset_text_field="text",
+    processing_class=tokenizer,
+    args=training_arguments
+)
+
+train_result = trainer.train()
