f062bf0cf4aded374988df046ba94d4dc4f7d7f8
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2025-02-06 20:46:29 +0900

    Begin llama-trainer.py

---
+++ llama-trainer.py
@@ -0,0 +1,10 @@
+import transformers, datasets, torch
+import bitsandbytes as bnb
+import gc
+import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import argparse, sys, os, re, time, unicodedata, pickle, html
+import sacrebleu
+import wandb
