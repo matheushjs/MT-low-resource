ad9ad6123394cbd477797706483b445c8ad3d226
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2025-02-06 20:46:54 +0900

    More imports and pkldump, pklload

--- llama-trainer.py
+++ llama-trainer.py
@@ -8,3 +8,26 @@ import matplotlib.pyplot as plt
 import argparse, sys, os, re, time, unicodedata, pickle, html
 import sacrebleu
 import wandb
+from iso639 import Language
+from pathlib import Path
+from sacremoses import MosesPunctNormalizer
+from transformers import (
+    AutoTokenizer, AutoConfig, AutoModelForSeq2SeqLM, AutoModelForCausalLM,
+    get_constant_schedule_with_warmup, BitsAndBytesConfig, logging, EarlyStoppingCallback
+)
+from transformers.optimization import Adafactor
+from datasets import load_dataset, Dataset, DatasetDict
+from peft import LoraConfig, PeftModel, get_peft_model
+from trl import SFTTrainer, SFTConfig, setup_chat_format
+from datetime import datetime as dt
+from comet import download_model, load_from_checkpoint
+from torch.utils.data import DataLoader, Dataset as torchDataset
+
+def pkldump(obj, file):
+    with open(file, "wb") as fp:
+        pickle.dump(obj, fp)
+
+def pklload(file):
+    with open(file, "rb") as fp:
+        return pickle.load(fp)
+
