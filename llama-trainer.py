ba6fb693acbb61adb509de12678c43689a0ef2a7
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2025-02-06 20:47:39 +0900

    Add parsing

--- llama-trainer.py
+++ llama-trainer.py
@@ -31,3 +31,66 @@ def pklload(file):
     with open(file, "rb") as fp:
         return pickle.load(fp)
 
+parser = argparse.ArgumentParser(prog='Llama Model Trainer')
+parser.add_argument("--lang-pairs",
+        help="Comma-separated language pairs to use. Direction matters.",
+        required=True,
+        type=lambda x: x.split(","))
+parser.add_argument("--training-steps",
+        help="Number of training steps with various language pairs.",
+        type=int,
+        default=20000)
+parser.add_argument("--post-training-steps",
+        help="Number of training steps with only hy-en language pair.",
+        type=int,
+        default=20000)
+parser.add_argument("--epochs",
+        help="Number of epochs to train.",
+        type=int,
+        default=5)
+parser.add_argument("--main-lang-pair",
+        help="Main language pair. Direction matters.",
+        default="en-hy")
+parser.add_argument("--load-existing",
+        help="Load specific checkpoint.",
+        default="",
+        type=str)
+parser.add_argument("--skip-test",
+        help="Should we test the model?",
+        action='store_true')
+parser.add_argument("--patience",
+        help="Patience of early stopping. Applied to both pre- and post-training.",
+        type=int,
+        default=50)
+parser.add_argument("--post-patience",
+        help="Patience of early stopping in post-training.",
+        type=int,
+        default=50)
+parser.add_argument("--batch-size",
+        help="Size of batches to process in the GPU. Increases GPU memory requirement.",
+        type=int,
+        default=16)
+parser.add_argument("--tok-max-length",
+        help="Truncate token sequences to this length. Increases GPU memory requirement.",
+        type=int,
+        default=256)
+parser.add_argument("--limit-train-corpus",
+        help="Truncates training corpus for each language to this number of sentences.",
+        type=int,
+        default=-1)
+parser.add_argument("--limit-test-samples",
+        help="Tests on only the first N samples.",
+        type=int,
+        default=-1)
+parser.add_argument("--middle-limit-test-samples",
+        help="Middle-tests on only the first N samples.",
+        type=int,
+        default=-1)
+parser.add_argument("--gradient-accumulation-steps",
+        help="Number of steps to accumulate the gradients before updating weights.",
+        type=int,
+        default=2)
+
+#args = parser.parse_args(["--lang-pairs", "en-ko,en-hy"])
+args = parser.parse_args()
+
