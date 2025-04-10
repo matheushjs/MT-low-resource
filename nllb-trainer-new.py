f85bed5771ac3ca9a92a11039227fcf06728875d
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2025-04-10 17:08:13 +0900

    Finish post-training

--- nllb-trainer-new.py
+++ nllb-trainer-new.py
@@ -644,3 +644,85 @@ if __name__ == "__main__":
         print(test_scores[0])
         print(test_scores[1])
 
+    time_after_train = time.time()
+
+    if args.post_training_steps > 0:
+        run = wandb.init(
+            entity="elfmathews-university-of-tsukuba",
+            project="posttrain-" + EXPERIMENT_NAME, 
+            job_type="training", 
+            anonymous="allow"
+        )
+
+        # optimizer = AdamW(
+        #     [p for p in model.parameters() if p.requires_grad],
+        #     lr=args.learning_rate / 10,
+        #     weight_decay=args.weight_decay
+        # )
+        # scheduler = get_constant_schedule_with_warmup(optimizer, args.warmup_ratio)
+        
+        # Data collator for seq2seq
+        data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
+
+        epoch_batch_count = len(train_dataset) / (args.batch_size * args.gradient_accumulation_steps)
+        warmup_steps = int(args.warmup_ratio * epoch_batch_count)
+
+        if args.post_learning_rate < 0:
+            post_lr = args.learning_rate / 10
+        else:
+            post_lr = args.post_learning_rate
+
+        #Hyperparamter
+        training_arguments = Seq2SeqTrainingArguments(
+            output_dir=POST_MODEL_SAVE_PATH,
+            overwrite_output_dir=True,
+            per_device_train_batch_size=args.batch_size,
+            per_device_eval_batch_size=args.batch_size,
+            gradient_accumulation_steps=args.gradient_accumulation_steps,
+            optim="adamw_torch",
+            num_train_epochs=args.epochs,
+            max_steps=args.post_training_steps,
+            eval_strategy="steps",
+            eval_steps=args.post_eval_steps,
+            eval_accumulation_steps=50,
+            save_strategy="steps",
+            save_steps=args.post_eval_steps,
+            save_total_limit=1,
+            load_best_model_at_end=True,
+            logging_steps=1,
+            logging_strategy="steps",
+            learning_rate=post_lr,
+            fp16=True,
+            bf16=False,
+            weight_decay=args.weight_decay,
+            group_by_length=False,
+            #max_seq_length=args.tok_max_length,
+            max_grad_norm=args.max_grad_norm,
+            warmup_steps=warmup_steps,
+            lr_scheduler_type="constant_with_warmup",
+            dataloader_num_workers=4,
+            #max_length=512, # Idk what's going on. This is the latest version of TRL but it doesn't seem like it.
+            report_to="wandb",
+            save_safetensors=False
+        )
+
+        # Setting sft parameters
+        trainer = Seq2SeqTrainer(
+            model=model,
+            train_dataset=post_train_dataset,
+            eval_dataset=post_dev_dataset,
+            #max_length=args.tok_max_length,
+            #dataset_text_field="text",
+            processing_class=tokenizer,
+            data_collator=data_collator,
+            args=training_arguments,
+            #optimizers=(optimizer, scheduler),
+            callbacks=[EarlyStoppingCallback(args.post_patience)]
+        )
+
+        model.config.use_cache = False
+        try:
+            post_train_result = trainer.train()
+        except KeyboardInterrupt:
+            print("Caught Ctrl+C or SIGINT. Interrupting post-training and proceeding to scoring.")
+
