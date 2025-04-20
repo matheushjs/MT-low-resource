6ce971ae2f42d85423a319258b9ad7af531bf746
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2025-04-20 17:46:50 +0900

    Begin parse-report.py

---
+++ parse-report.py
@@ -0,0 +1,393 @@
+text = """
+output-llama-fullfromscratch4-2025-06-03-21-57-03.txt
+hy-en ro-en
+BLEU = 15.84
+chrF2++ = 31.25
+BLEU = 16.17
+chrF2++ = 31.80
+COMET system score: 0.7781134637441929
+(comp_test) BLEU = 29.25
+(comp_test) chrF2++ = 55.69
+COMET system score: 0.7914105258740548
+Full test dataset average BLEU: 25.93983309005442
+Full test dataset average chrF2++: 49.64882676787974
+Full test dataset average COMET: 0.7880472533144808
+
+output-llama-fullfromscratch4-2025-06-03-21-57-49.txt
+hy-en cs-en
+BLEU = 16.38
+chrF2++ = 32.36
+BLEU = 16.52
+chrF2++ = 32.04
+COMET system score: 0.7789931118488311
+(comp_test) BLEU = 28.54
+(comp_test) chrF2++ = 54.55
+COMET system score: 0.7886522072814838
+Full test dataset average BLEU: 25.047876889786927
+Full test dataset average chrF2++: 48.00938051568842
+Full test dataset average COMET: 0.7858449196201585
+
+output-llama-fullfromscratch4-2025-06-03-21-58-03.txt
+hy-en hr-en
+BLEU = 16.15
+chrF2++ = 31.79
+BLEU = 15.89
+chrF2++ = 31.84
+COMET system score: 0.7742607406320026
+(comp_test) BLEU = 29.98
+(comp_test) chrF2++ = 56.56
+COMET system score: 0.7895519328196464
+Full test dataset average BLEU: 26.553171115018703
+Full test dataset average chrF2++: 50.54958846917201
+Full test dataset average COMET: 0.7858321945319939
+
+output-llama-fullfromscratch4-2025-06-04-06-53-59.txt
+hy-en tr-en
+BLEU = 16.01
+chrF2++ = 31.74
+BLEU = 16.58
+chrF2++ = 32.27
+COMET system score: 0.7818361401295347
+(comp_test) BLEU = 21.81
+(comp_test) chrF2++ = 48.53
+COMET system score: 0.7829420902741016
+Full test dataset average BLEU: 20.56605281955809
+Full test dataset average chrF2++: 44.66360547009736
+Full test dataset average COMET: 0.782679156048555
+
+output-llama-fullfromscratch4-2025-06-04-06-58-27.txt
+hy-en hu-en
+BLEU = 16.05
+chrF2++ = 32.41
+BLEU = 16.37
+chrF2++ = 32.21
+COMET system score: 0.7802328943681087
+(comp_test) BLEU = 24.16
+(comp_test) chrF2++ = 50.21
+COMET system score: 0.7802163156873692
+Full test dataset average BLEU: 22.292326613854495
+Full test dataset average chrF2++: 45.89725695078547
+Full test dataset average COMET: 0.7802202888363047
+
+output-llama-fullfromscratch4-2025-06-04-07-27-05.txt
+hy-en ru-en
+BLEU = 16.16
+chrF2++ = 31.49
+BLEU = 16.09
+chrF2++ = 32.17
+COMET system score: 0.7789597216419186
+(comp_test) BLEU = 23.12
+(comp_test) chrF2++ = 49.82
+COMET system score: 0.7648443662515984
+Full test dataset average BLEU: 21.559381288893228
+Full test dataset average chrF2++: 45.89806904873214
+Full test dataset average COMET: 0.7679829094558641
+
+output-llama-fullfromscratch4-2025-06-04-16-46-25.txt
+az-en ru-en
+BLEU = 14.94
+chrF2++ = 38.53
+BLEU = 14.48
+chrF2++ = 38.43
+COMET system score: 0.7328740050283711
+(comp_test) BLEU = 23.33
+(comp_test) chrF2++ = 49.92
+COMET system score: 0.7649649382956017
+Full test dataset average BLEU: 22.084543473949783
+Full test dataset average chrF2++: 48.296545418132986
+Full test dataset average COMET: 0.7604400100567821
+
+output-llama-fullfromscratch4-2025-06-04-16-51-50.txt
+az-en ro-en
+BLEU = 14.61
+chrF2++ = 38.71
+BLEU = 15.53
+chrF2++ = 39.37
+COMET system score: 0.7474084991082717
+(comp_test) BLEU = 32.02
+(comp_test) chrF2++ = 57.30
+COMET system score: 0.803788261378704
+Full test dataset average BLEU: 29.334941699198463
+Full test dataset average chrF2++: 54.38258607602614
+Full test dataset average COMET: 0.7946130343133242
+
+output-llama-fullfromscratch4-2025-06-04-18-14-57.txt
+az-en cs-en
+BLEU = 15.26
+chrF2++ = 39.14
+BLEU = 14.95
+chrF2++ = 38.48
+COMET system score: 0.7360040298003829
+(comp_test) BLEU = 27.94
+(comp_test) chrF2++ = 54.18
+COMET system score: 0.7850819432984059
+Full test dataset average BLEU: 25.464449849502074
+Full test dataset average chrF2++: 51.188169182717914
+Full test dataset average COMET: 0.7757362666737555
+
+output-llama-fullfromscratch4-2025-06-04-23-22-26.txt
+hy-en ko-en
+BLEU = 16.55
+chrF2++ = 32.09
+BLEU = 16.25
+chrF2++ = 31.53
+COMET system score: 0.778834907047549
+(comp_test) BLEU = 16.33
+(comp_test) chrF2++ = 41.60
+COMET system score: 0.7636744839509841
+Full test dataset average BLEU: 16.31527022925831
+Full test dataset average chrF2++: 39.41052753120793
+Full test dataset average COMET: 0.7669735552698104
+
+output-llama-fullfromscratch4-2025-06-05-00-07-57.txt
+az-en ko-en
+BLEU = 14.89
+chrF2++ = 39.24
+BLEU = 14.94
+chrF2++ = 39.15
+COMET system score: 0.742267629470718
+(comp_test) BLEU = 16.82
+(comp_test) chrF2++ = 42.23
+COMET system score: 0.7682553048050059
+Full test dataset average BLEU: 16.563687364823153
+Full test dataset average chrF2++: 41.80391787141605
+Full test dataset average COMET: 0.7646770296198953
+
+output-llama-fullfromscratch4-2025-06-05-00-44-24.txt
+az-en hr-en
+BLEU = 14.45
+chrF2++ = 38.29
+BLEU = 14.88
+chrF2++ = 39.14
+COMET system score: 0.7439882478017485
+(comp_test) BLEU = 32.21
+(comp_test) chrF2++ = 57.62
+COMET system score: 0.7987591144259035
+Full test dataset average BLEU: 29.51130597016446
+Full test dataset average chrF2++: 54.741955110404106
+Full test dataset average COMET: 0.7902260068612731
+
+output-llama-fullfromscratch4-2025-06-05-07-29-43.txt
+az-en tr-en
+BLEU = 15.23
+chrF2++ = 39.77
+BLEU = 15.17
+chrF2++ = 38.85
+COMET system score: 0.739748263057698
+(comp_test) BLEU = 22.03
+(comp_test) chrF2++ = 47.96
+COMET system score: 0.7836970314308627
+Full test dataset average BLEU: 20.986204356860874
+Full test dataset average chrF2++: 46.57940855646594
+Full test dataset average COMET: 0.7770226914580503
+
+output-llama-fullfromscratch4-2025-06-05-07-31-07.txt
+az-en hu-en
+BLEU = 13.98
+chrF2++ = 37.30
+BLEU = 14.85
+chrF2++ = 37.69
+COMET system score: 0.740069124705336
+(comp_test) BLEU = 24.84
+(comp_test) chrF2++ = 50.31
+COMET system score: 0.7824287896513527
+Full test dataset average BLEU: 23.3088673321403
+Full test dataset average chrF2++: 48.37574174660598
+Full test dataset average COMET: 0.7759382545753596
+
+output-llama-fullfromscratch4-2025-06-05-10-29-08.txt
+ka-en hu-en
+BLEU = 13.85
+chrF2++ = 27.92
+BLEU = 13.24
+chrF2++ = 27.40
+COMET system score: 0.7477081171548386
+(comp_test) BLEU = 24.35
+(comp_test) chrF2++ = 50.69
+COMET system score: 0.7822057617149306
+Full test dataset average BLEU: 22.580327931170192
+Full test dataset average chrF2++: 46.98986860669654
+Full test dataset average COMET: 0.7767203121700211
+
+output-llama-fullfromscratch4-2025-06-05-14-31-19.txt
+ka-en ru-en
+BLEU = 12.84
+chrF2++ = 26.45
+BLEU = 13.03
+chrF2++ = 27.01
+COMET system score: 0.7403172995487979
+(comp_test) BLEU = 21.28
+(comp_test) chrF2++ = 48.30
+COMET system score: 0.755774675849396
+Full test dataset average BLEU: 20.073992675573663
+Full test dataset average chrF2++: 45.18547524722669
+Full test dataset average COMET: 0.7535116128936078
+
+output-llama-fullfromscratch4-2025-06-05-14-44-32.txt
+ka-en ro-en
+BLEU = 13.04
+chrF2++ = 27.23
+BLEU = 13.26
+chrF2++ = 27.04
+COMET system score: 0.7410572692873427
+(comp_test) BLEU = 31.75
+(comp_test) chrF2++ = 57.22
+COMET system score: 0.801638121806334
+Full test dataset average BLEU: 28.629293400382846
+Full test dataset average chrF2++: 52.12819750328317
+Full test dataset average COMET: 0.7914113269386677
+
+output-llama-fullfromscratch4-2025-06-05-20-16-10.txt
+ka-en cs-en
+BLEU = 13.03
+chrF2++ = 27.29
+BLEU = 13.24
+chrF2++ = 27.64
+COMET system score: 0.7445191843085732
+(comp_test) BLEU = 28.00
+(comp_test) chrF2++ = 54.72
+COMET system score: 0.7876420635356319
+Full test dataset average BLEU: 25.083755334369155
+Full test dataset average chrF2++: 49.38086625327337
+Full test dataset average COMET: 0.7791344558729085
+
+output-llama-fullfromscratch4-2025-06-05-23-19-37.txt
+be-en cs-en
+BLEU = 23.11
+chrF2++ = 49.10
+BLEU = 22.27
+chrF2++ = 47.31
+COMET system score: 0.7453364676401659
+(comp_test) BLEU = 26.60
+(comp_test) chrF2++ = 52.56
+COMET system score: 0.7780178026104426
+Full test dataset average BLEU: 25.96017799270951
+Full test dataset average chrF2++: 51.7856812170407
+Full test dataset average COMET: 0.7731998250264832
+
+output-llama-fullfromscratch4-2025-06-05-23-21-02.txt
+ka-en ko-en
+BLEU = 12.81
+chrF2++ = 26.69
+BLEU = 13.08
+chrF2++ = 27.27
+COMET system score: 0.7458581388677313
+(comp_test) BLEU = 17.06
+(comp_test) chrF2++ = 42.28
+COMET system score: 0.7705841530057634
+Full test dataset average BLEU: 16.488734284986773
+Full test dataset average chrF2++: 40.135931108738205
+Full test dataset average COMET: 0.767048610207037
+
+output-llama-fullfromscratch4-2025-06-06-04-59-45.txt
+ka-en hr-en
+BLEU = 13.33
+chrF2++ = 27.63
+BLEU = 13.58
+chrF2++ = 27.41
+COMET system score: 0.7459962875574682
+(comp_test) BLEU = 31.63
+(comp_test) chrF2++ = 57.66
+COMET system score: 0.7972180225061674
+Full test dataset average BLEU: 28.713018848146156
+Full test dataset average chrF2++: 52.76701827428233
+Full test dataset average COMET: 0.7889375163007084
+
+output-llama-fullfromscratch4-2025-06-06-05-41-44.txt
+ka-en tr-en
+BLEU = 13.02
+chrF2++ = 27.01
+BLEU = 13.31
+chrF2++ = 27.36
+COMET system score: 0.7402367709997623
+(comp_test) BLEU = 21.99
+(comp_test) chrF2++ = 48.51
+COMET system score: 0.7879870895944049
+Full test dataset average BLEU: 20.618200775894188
+Full test dataset average chrF2++: 45.17460359348525
+Full test dataset average COMET: 0.7804611802306648
+
+output-llama-fullfromscratch4-2025-06-06-09-19-02.txt
+be-en hr-en
+BLEU = 23.56
+chrF2++ = 49.49
+BLEU = 23.00
+chrF2++ = 47.95
+COMET system score: 0.7461506685047793
+(comp_test) BLEU = 32.14
+(comp_test) chrF2++ = 57.51
+COMET system score: 0.7964924029942595
+Full test dataset average BLEU: 31.051813227590163
+Full test dataset average chrF2++: 56.3655657398645
+Full test dataset average COMET: 0.7904785718633806
+
+output-llama-fullfromscratch4-2025-06-06-14-44-06.txt
+be-en tr-en
+BLEU = 22.93
+chrF2++ = 48.67
+BLEU = 22.74
+chrF2++ = 48.15
+COMET system score: 0.744945065375486
+(comp_test) BLEU = 21.88
+(comp_test) chrF2++ = 48.07
+COMET system score: 0.7821609538583899
+Full test dataset average BLEU: 21.977446766545576
+Full test dataset average chrF2++: 48.07585322201986
+Full test dataset average COMET: 0.7778319879631133
+
+output-llama-fullfromscratch4-2025-06-06-14-44-21.txt
+be-en hu-en
+BLEU = 22.92
+chrF2++ = 48.84
+BLEU = 23.54
+chrF2++ = 49.19
+COMET system score: 0.7529797380321597
+(comp_test) BLEU = 24.74
+(comp_test) chrF2++ = 50.54
+COMET system score: 0.7815875289558419
+Full test dataset average BLEU: 24.600030741830118
+Full test dataset average chrF2++: 50.380673484469625
+Full test dataset average COMET: 0.7782288440562161
+
+output-llama-fullfromscratch4-2025-06-06-16-29-25.txt
+be-en ru-en
+BLEU = 23.04
+chrF2++ = 48.84
+BLEU = 22.62
+chrF2++ = 48.13
+COMET system score: 0.7456802329037087
+(comp_test) BLEU = 23.18
+(comp_test) chrF2++ = 49.45
+COMET system score: 0.7624227875608452
+Full test dataset average BLEU: 23.118047489205303
+Full test dataset average chrF2++: 49.30778335386793
+Full test dataset average COMET: 0.7606199972763763
+
+output-llama-fullfromscratch4-2025-06-06-20-44-11.txt
+be-en ro-en
+BLEU = 23.21
+chrF2++ = 49.35
+BLEU = 20.82
+chrF2++ = 45.27
+COMET system score: 0.7270191924109415
+(comp_test) BLEU = 29.51
+(comp_test) chrF2++ = 54.69
+COMET system score: 0.7881396809172823
+Full test dataset average BLEU: 28.419293081505316
+Full test dataset average chrF2++: 53.51369538362008
+Full test dataset average COMET: 0.7804981615487668
+
+output-llama-fullfromscratch4-2025-06-06-21-24-22.txt
+gl-en ro-en
+BLEU = 33.19
+chrF2++ = 59.49
+BLEU = 33.38
+chrF2++ = 59.29
+COMET system score: 0.8038074204799395
+(comp_test) BLEU = 31.83
+(comp_test) chrF2++ = 56.93
+COMET system score: 0.8002336756211393
+Full test dataset average BLEU: 32.107371152798876
+Full test dataset average chrF2++: 57.35300801279808
+Full test dataset average COMET: 0.8008705172112518
+"""
